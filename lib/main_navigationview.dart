e42325650a37e6c6203096d5d57159ff2d7c2b4c
ABDUL AZIZ BIN MABENI <52974941+azizben345@users.noreply.github.com>
2025-05-21 11:17:19 +0800

    Add files via upload
    main navigation for dashboard

---
+++ lib/main_navigationview.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'ui/dashboard/view_dashboard.dart';
+import 'ui/dashboard/schedule_page.dart';
+import 'ui/profile/profile_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MainNavigationView(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MainNavigationView extends StatefulWidget {
+  const MainNavigationView({super.key});
+
+  @override
+  State<MainNavigationView> createState() => _MainNavigationViewState();
+}
+
+class _MainNavigationViewState extends State<MainNavigationView> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const DashboardView(),
+    const SchedulePage(),
+    const ProfilePage(), // Create this file
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_getTitle()),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/profile_pic_default.jpg'),
+            ),
+          ),
+        ],
+      ),
+      body: IndexedStack(
+        // Content will be displayed here:
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule),
+            label: 'Schedule',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getTitle() {
+    switch (_selectedIndex) {
+      case 1:
+        return 'Schedule';
+      case 2:
+        return 'Profile';
+      default:
+        return 'Dashboard';
+    }
+  }
+}
