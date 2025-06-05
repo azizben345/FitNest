3c4fe58a1d92762ecc0b53ab6239780e809bcf81
ABDUL AZIZ BIN MABENI <52974941+azizben345@users.noreply.github.com>
2025-06-05 12:27:17 +0800

    Add files via upload
    firebase configure [android folder]

---
+++ android/app/src/main/kotlin/com/example/fitnest_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fitnest_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    // Remove the version specification for google-services here
+    // id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Make sure the version of google-services plugin is consistent
+        classpath("com.google.gms:google-services:4.3.10") // Update the version to match across the project
+        classpath("com.android.tools.build:gradle:8.4.2")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
