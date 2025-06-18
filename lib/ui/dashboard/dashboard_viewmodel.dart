6cd32424b6a8c7cfcd9eb5604c2c3a5ccab43ffd
faiz03-glicth <ahmadfaiz32146@gmail.com>
2025-06-18 11:52:57 +0800

    comit

--- lib/ui/dashboard/dashboard_viewmodel.dart
+++ lib/ui/dashboard/dashboard_viewmodel.dart
@@ -10,6 +10,10 @@ import 'package:intl/intl.dart';
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+import 'dart:math';
+>>>>>>> Stashed changes
 =======
 import 'dart:math';
 >>>>>>> Stashed changes
@@ -140,11 +144,14 @@ class DashboardViewModel {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
           .doc(user.uid) // Document for the specific user
           .collection('meals') // Subcollection of meals for that user
           .orderBy('mealTime', descending: true)
           .limit(30) // Fetch enough data for the chart (e.g., last 30 entries)
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -173,6 +180,9 @@ class DashboardViewModel {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -206,8 +216,12 @@ class DashboardViewModel {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
           'mealTime':
               doc.data()?['mealTime'] as Timestamp?, // Keep as Timestamp!
+=======
+          'mealTime': doc.data()?['mealTime'] as Timestamp?,
+>>>>>>> Stashed changes
 =======
           'mealTime': doc.data()?['mealTime'] as Timestamp?,
 >>>>>>> Stashed changes
