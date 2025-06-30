89aff489490467ae918ec5b5e1924ceb63690498
faiz03-glicth <ahmadfaiz32146@gmail.com>
2025-07-01 00:58:25 +0800

    commit

--- lib/ui/dashboard/dashboard_page.dart
+++ lib/ui/dashboard/dashboard_page.dart
@@ -6,8 +6,8 @@ import 'view_model/dashboard_viewmodel.dart';
 import 'view_model/schedule_viewmodel.dart';
 import 'package:fitnest_app/ui/activity/activity.dart';
 import 'package:fitnest_app/ui/activity/temp_activity_page.dart';
-import 'package:fitnest_app/ui/activity/nutritionIntake.dart';
-import 'package:fitnest_app/ui/activity/nutritionIntake_viewmodel.dart';
+import 'package:fitnest_app/ui/log/nutritionIntake.dart';
+import 'package:fitnest_app/ui/log/nutritionIntake_viewmodel.dart';
 import 'package:fitnest_app/ui/services/CalorieCalculatorService.dart';
 import 'package:fitnest_app/ui/dashboard/widgets/CaloriePieChart.dart';
 
